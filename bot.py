2df05499435e9aaa98dee70a82fe4251ac733b3d
IvesCostaBr <ivespauiniam@gmail.com>
2021-08-07 19:18:35 -0500

    initial commit|

---
+++ bot.py
@@ -0,0 +1,39 @@
+from typing import Text
+import slack
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+
+
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+app = Flask(__name__)
+
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGINING_SECRET'],'/slack/events',app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+
+
+
+@slack_event_adapter.on('message')
+def message(payload):
+    print(payload)
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text_event = event.get('text')
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id , text=text_event)
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
